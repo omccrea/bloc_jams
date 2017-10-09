2b42ee0828b15c40bb48db258f9415a85003a145
Olivia McCrea <mccrea.olivia@gmail.com>
2017-10-09 11:58:38 -0700

    Checkpoint 24 DOM Play/Pause

--- scripts/album.js
+++ scripts/album.js
@@ -32,8 +32,8 @@ var albumMarconi = {
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
-       '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+        '<tr class="album-view-song-item">'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -69,6 +69,27 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+// Elements to add listeners to
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+    // #1 target property stores the DOM element where the event occured
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             //Selects first child element - song-item-number
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
 };
