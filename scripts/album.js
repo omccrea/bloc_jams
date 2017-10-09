b9e3d09831865383211db5d3caed04bfacfcfe2f
Olivia McCrea <mccrea.olivia@gmail.com>
2017-10-09 11:39:16 -0700

    Assignment 24 DOM Album View: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@ var albumMarconi = {
     ]
 };
 
+// Assignment (third) Example Album
+var albumAltj = {
+    title: 'An Awesome Wave',
+    artist: 'Alt-J',
+    label: 'Atlantic',
+    year: '2012',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Matilda', duration: '3:48' },
+        { title: 'Something Good', duration: '5:01' },
+        { title: 'Breezeblocks', duration: '3:21'},
+        { title: 'Dissolve Me', duration: '3:14' },
+        { title: 'Taro', duration: '2:15'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -42,15 +58,15 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+// #1 select all of the HTML elements required on album page. Populated by assigning corresponding object values to HTML elements.
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 //Function = setCurrentAlbum. Takes album objects as arguments, injects that object's stored information into the template.
 var setCurrentAlbum = function(album) {
-    // #1 select all of the HTML elements required on album page. Populated by assigning corresponding object values to HTML elements.
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
     // #2  firstChild property identifies first child node of element, nodoeValue returns/sets that value.
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -71,4 +87,18 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+// Make array - easy access with index
+    var albums = [albumPicasso, albumMarconi, albumAltj];
+//starting value for index, assigned
+// declared outside otherwise every time the user would click, it would return to starting value. Basically, the index is remembered.
+// start at index 1 (not zero) so that when the user clicks on the album, it'll do something (move forward) instead of looking like nothing happened.
+    var index = 1;
+    albumImage.addEventListener("click", function(event)
+        setCurrentAlbum(album[index]);
+        index++;
+        if (index == album.length) {
+          index = 0;
+        }
+    ));
 };
