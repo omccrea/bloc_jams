014e3e9c2db363650e903e6d004279f017635c31
Olivia McCrea <mccrea.olivia@gmail.com>
2017-10-10 13:55:02 -0700

    Assignment 26 DOM Play/Pause 2: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -69,16 +69,32 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+// Added for ASGMT 26
+var child = document.getElementsByClassName('album-view-title')[0];
+var noParent  = document.querySelector('html');
+
 // Added in CKPT 26
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
+  var currentParent = element.parentElement;
+
+  if (currentParent) {
+    while (currentParent.className && currentParent.className !== targetClass) {
+      currentParent == currentParent.parentElement;
     }
+    if (currentParent.className == targetClass) {
+      return currentParent;
+    } else {
+        alert("No parent with that class name found.");
+    }
+  } else {
+      alert("No parent found.");
+  }
+
 };
+
+findParentByClassName(child, 'album-view');
+
+//STOP FOR ASGMT 26
 
 var getSongItem = function(element) {
     switch (element.className) {
